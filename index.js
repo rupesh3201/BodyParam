b2f786426e5f6aa70ebf490e8d029509afd04aa6
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-02-11 19:33:06 +0530

    student data fetch sucessfully

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Studnet = [
 { id : 1 , name : "Rupesh" , City : "Pune"},
 { id : 2 , name : "Nilesh" , City : "Nanded"},
 { id : 3 , name : "Pankaj" , City : "Goa"},
-{ id : 1 , name : "Sandip" , City : "Hingoli"},
+{ id : 4 , name : "Sandip" , City : "Hingoli"},
 
 
 
@@ -18,7 +18,7 @@ app.get("/Student",(req, res)=>{
     res.json({
         sucess : true,
         data : Studnet,
-        message : `Studnt fetch sucessfully  `,
+        message : `Student fetch sucessfully  `,
     });
 })
 const Port = 5002;
