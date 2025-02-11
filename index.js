5b60e235fa23392ad31b70293de8309107575ccc
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-02-11 19:20:25 +0530

    Api fetch sucessfully

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/Student",(req, res)=>{
         message : `Studnt fetch sucessfully  `,
     });
 })
-const Port = 5001;
+const Port = 5002;
 app.listen(Port,()=>
 {
     console.log(`server is running on ${Port}`);
